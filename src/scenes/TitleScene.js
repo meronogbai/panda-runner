06b42195f37af6cfce2846de456d63991a6507cb
Meron Ogbai <okbaymeron@gmail.com>
2021-01-15 15:17:39 +0300

    Add leaderboard button to title scene

--- src/scenes/TitleScene.js
+++ src/scenes/TitleScene.js
@@ -8,6 +8,7 @@ export default class TitleScene extends Phaser.Scene {
   preload() {
     this.load.image('start', 'assets/start.png');
     this.load.image('logo', 'assets/panda_title.png');
+    this.load.image('leaderboard', 'assets/leaderboard.png');
   }
 
   create() {
@@ -16,10 +17,16 @@ export default class TitleScene extends Phaser.Scene {
     // title
     this.add.text(this.scale.width * 0.5, 80, 'Panda Runner', { fontSize: 70, color: '#1babab' }).setOrigin();
     // start button
-    const startButton = this.add.image(this.scale.width * 0.5, this.scale.height * 0.5, 'start').setScale(0.5);
+    const startButton = this.add.image(this.scale.width * 0.5, this.scale.height * 0.5, 'start').setScale(0.6);
     startButton.setInteractive({ useHandCursor: true });
-    startButton.once('pointerdown', () => {
+    startButton.on('pointerdown', () => {
       this.scene.start('game');
+    });
+    // leaderboard
+    const leaderboardButton = this.add.image(this.scale.width * 0.5, this.scale.height * 0.5 + 100, 'leaderboard').setScale(0.4);
+    leaderboardButton.setInteractive({ useHandCursor: true });
+    leaderboardButton.on('pointerdown', () => {
+
     });
     // credits
     this.add.text(this.scale.width * 0.5, this.scale.height - 100, 'Created by: Meron Ogbai', { fontSize: 24, color: '#1babab' });
